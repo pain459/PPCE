c8a3a85db89bbcd700cbb68816ac61453b2fa2b7
Ravi <ravi.ameer@gmail.com>
2023-08-07 22:16:59 +0530

    docstrings demo.

---
+++ Core_Python_Programming/C3/docstrings.py
@@ -0,0 +1,20 @@
+# program with 2 functions
+def add(x, y):
+    """
+    This function takes two numbers and finds their sum.
+    It displays the sum as result
+    """
+    print("Sum =", (x + y))
+
+
+def message():
+    """
+    This function displays a message.
+    This is welcome message to the user.
+    """
+    print("Welcome to Python!")
+
+
+# calling the functions
+add(10, 25)
+message()
