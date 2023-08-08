af44aace6fec9e0be185039087277fc9b3d25eee
Ravi <ravi.ameer@gmail.com>
2023-08-08 06:12:29 +0530

    some more basics

---
+++ Core_Python_Programming/C3/string_to_decimal.py
@@ -0,0 +1,12 @@
+# Python program to convert into decimal number system
+s1 = "17"
+s2 = "1110010"
+s3 = "1c2"
+
+# radix limit is from 2 to 36
+n = int(s1, 8)
+print(n)
+n = int(s2, 2)
+print(n)
+n = int(s3, 16)
+print(n)
