b0a00e7a221e274ab1557644497d691b7c25fd2d
Ravi <ravi.ameer@gmail.com>
2023-08-08 06:41:25 +0530

    Inputs and formats.

---
+++ Core_Python_Programming/C5/p9.py
@@ -0,0 +1,6 @@
+# Find sum and product of 2 numbers
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("The sum of {0} and {1} is {2}" .format(x, y, (x + y)))
+print("The product of {0} and {1} is {2}" .format(x, y, (x * y)))
+print("The sum of {0} and {1} is {2} and product is {3}" .format(x, y, x + y, x * y))
