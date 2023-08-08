be5eed04f8f8767e7b6173f60e3b1594153362d1
Ravi <ravi.ameer@gmail.com>
2023-08-08 05:34:28 +0530

    data type conversion to int.

---
+++ Core_Python_Programming/C3/convert_into_decimals.py
@@ -0,0 +1,15 @@
+# python program to convert into decimal number system
+n1 = 0o17
+n2 = 0B1110010
+n3 = 0X1c2
+
+n = int(n1)
+print(n)
+n = int(n2)
+print(n2)
+n = int(n3)
+print(n)
+
+str = "1C2"
+n = int(str, 16)  # displaying to the base 16.
+print(n)
